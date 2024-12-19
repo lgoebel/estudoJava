4cc66850a6e9a80b66701bee25a989fcd020c77d
Lucas Goebel <pro.lcsgoebel@gmail.com>
2024-12-18 21:30:46 -0300

    Primeiros comandos Java

---
+++ Ninja.java
@@ -0,0 +1,5 @@
+public class Ninja {
+    String nome;
+    String aldeia;
+    int idade;
+}
---
+++ main.java
@@ -0,0 +1,18 @@
+public class main {
+    public static void main(String[] args) {
+
+        //Criando um objeto
+        Ninja naruto = new Ninja();
+        naruto.nome = "Naruto Uzumaki";
+        naruto.aldeia = "Aldeia da Folha";
+        naruto.idade = 12;
+
+        Ninja sasuke = new Ninja();
+        sasuke.nome = "Sasuke Uchiha";
+        sasuke.aldeia = "Aldeia da Folha";
+        sasuke.idade = 13;
+
+        System.out.println(sasuke.nome);
+
+    }
+}
